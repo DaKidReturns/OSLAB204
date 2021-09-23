00e47c858d7bb0d8fe80a4060215deb29a67bebb
Rohit Karunakaran <rohitkarun@gmail.com>
2021-09-23 07:47:11 +0530

    changes to shm

--- Experiment_11/shm.c
+++ Experiment_11/shm.c
@@ -4,25 +4,28 @@
 #include<stdlib.h>
 
 int main(){
-    key_t token = ftok("Token",65);
-    int shmld = shmget(token, 1024,0666|IPC_CREAT);
-    char* str = (char*) shmat (shmld, (void*)0, 0);
+    key_t token = ftok("SHAREDMEM",69);
+    int shmId = shmget(token, 1024, 0666|IPC_CREAT);
+    char* str = (char*) shmat (shmId, (void*)0, 0);
+    //int* str = (int*) shmat (shmld, (void*)0, 0);
 
-    //char str[1024] ;
+    char str[1024] ;
     int choice;
     while(1) {
-        printf("\n1. Producer\n2. Consumer\n3. Stop\nEnter Choice: ");
+        printf("\n1. Read\n2. Write\n3. Stop\nEnter Choice: ");
         scanf("%d",&choice);
         switch(choice){
-            case 1: int data = rand();
-                    printf("Data for the shared memory: %d ", data);
+            case 1: int data;// = rand();
+                    printf("Data for the shared memory: ");//, data);
+                    scanf("%d",&data);
+                    *str = data;
                     sprintf(str,"%d",data);
                     break;
             case 2: printf("\nValue in the Shared Memory: %s\n",str);
                     break;
             case 3: printf("\nDetaching the shared memory\n");
                     shmdt(str);
-                    shmctl(shmld, IPC_RMID,NULL); 
+                    shmctl(shmId, IPC_RMID,NULL); 
                     exit(0);
                     break;
             default: printf("Invalid option!!! \nEnter 1/2/3\n");
